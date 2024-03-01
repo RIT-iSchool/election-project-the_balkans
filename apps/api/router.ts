3c9b9e2e908761e18279e607c30d65d69f5faf00
cgadd21 <ctg7866@g.rit.edu>
2024-03-01 15:03:11 -0500

    removed routes and moved to one route file

--- apps/api/router.ts
+++ apps/api/router.ts
@@ -1,3 +1,8 @@
 import { Router } from 'express';
+import * as elections from './src/services/election-service';
 
 export const router = Router();
+
+router.post('/v1/elections', elections.create);
+router.get('/v1/elections', elections.list);
+router.get('/v1/elections/:electionId', elections.retrieve);
--- apps/api/src/routes/v1-routes.ts
+++
@@ -1,6 +0,0 @@
-import { router } from '../../router';
-import * as elections from '../services/election-service';
-
-router.post('/v1/elections', elections.create);
-router.get('/v1/elections', elections.list);
-router.get('/v1/elections/:electionId', elections.retrieve);
