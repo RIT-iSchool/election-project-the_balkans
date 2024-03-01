888271be5734fbcad90c538a163e2f499f152b53
cgadd21 <ctg7866@g.rit.edu>
2024-03-01 16:38:34 -0500

    started login in dl

--- apps/api/src/data/user-data.ts
+++ apps/api/src/data/user-data.ts
@@ -1,7 +1,7 @@
-import { CreateUser } from '../db/schema';
+import { CreateUser, User } from '../db/schema';
 import { db } from '../db';
 import { user } from '../db/schema';
-import { eq } from 'drizzle-orm';
+import { eq, and } from 'drizzle-orm';
 
 export type Create = {
   userData: CreateUser;
@@ -36,5 +36,23 @@ export const retrieve = async ({ userId }: Retrieve) => {
     return userData;
   } catch (err) {
     throw new Error('Something went wrong retrieving a user');
+  }
+};
+
+/**
+ * Login a user.
+ */
+export const login = async (email: string, password: string): Promise<User> => {
+  try {
+    const [loginUser] = await db
+      .select()
+      .from(user)
+      .where(and(eq(user.email, email), eq(user.password, password)));
+
+    if (!loginUser) throw new Error('Wrong email or password');
+
+    return loginUser;
+  } catch (err) {
+    throw new Error('Wrong email or password');
   }
 };
