ffb5c4c80980f3b3a5db4797b1cf3a78c32dfe34
Connor Stevens <conmail1224@gmail.com>
2024-04-08 15:15:14 -0400

    Chore

--- apps/api/src/lib/logger.ts
+++ apps/api/src/lib/logger.ts
@@ -5,10 +5,6 @@ export const logger = bunyan.createLogger({
   name: 'american-dream-api',
   streams: [
     {
-      level: 'info',
-      stream: process.stdout,
-    },
-    {
       type: 'rotating-file',
       path: path.join(process.cwd(), 'logs', 'american-dream.log'),
       period: '1d',
