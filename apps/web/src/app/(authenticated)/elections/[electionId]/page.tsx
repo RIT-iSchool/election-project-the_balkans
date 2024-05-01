8e397372e5dc99e33da34b239f8c99608d77bf89
cgadd21 <ctg7866@g.rit.edu>
2024-05-01 03:33:32 -0400

    comment rm'd

--- apps/web/src/app/(authenticated)/elections/[electionId]/page.tsx
+++ apps/web/src/app/(authenticated)/elections/[electionId]/page.tsx
@@ -314,7 +314,6 @@ export default function Page({ params }: PageProps) {
             </div>
           </>
         )}
-        {/* election hasnt started */}
         {hasntStarted && (
           <>
             <div className="grid grid-cols-1 gap-4 px-6 md:grid-cols-2 lg:grid-cols-2">
